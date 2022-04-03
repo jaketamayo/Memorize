cc94d7db5140704ac40c72894ec6d04727611e96
Jake Tamayo <hellojtmatthew@icloud.com>
2022-04-03 18:54:15 -0500

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Jacob Tamayo on 4/3/22.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
